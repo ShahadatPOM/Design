1f26a63a4f1307f7f6601d2f40ce33171ad12dfa
ShahadatPOM <naiem.3112@gmail.com>
2020-06-20 14:29:39 +0600

    showing value using for loop in js file

--- JS/js2/main.js
+++ JS/js2/main.js
@@ -1,15 +1,20 @@
-function main(){
+function main() {
 	var a = parseInt(document.getElementById('first').value);
 	var b = parseInt(document.getElementById('second').value);
 	var c = parseInt(document.getElementById('third').value);
 
-	if(a>b && a>c){
+	if (a > b && a > c) {
 		document.write("<h3>Max value is A</h3>");
-	}
-	else if(b>a && b>c){
+	} else if (b > a && b > c) {
 		document.write("<h3>Max value is B</h3>");
-	}
-	else{
+	} else {
 		document.write("<h3>Max value is C</h3>");
 	}
 }
+
+document.write("<select>")
+document.write("<option> ---select year--- </option>");
+for (i = 2010; i <= 3000; i++) {
+	document.write("<option>" + i + "</option>");
+}
+document.write("</select")
