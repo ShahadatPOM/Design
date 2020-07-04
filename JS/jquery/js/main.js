1a276fe3205b049742d4e40f374152607d0c35aa
ShahadatPOM <naiem.3112@gmail.com>
2020-07-05 05:51:31 +0600

    sticky navbar

--- JS/jquery/js/main.js
+++ JS/jquery/js/main.js
@@ -15,9 +15,44 @@ $(function(){
 		$('h1').html("yo all work together").addClass('css').toggle(3000);
 	});
 
+// scrollTop
 	$('.topbtn').click(function(){
 		$("html,body").animate({
 			scrollTop : 0
 		}, 3000);
 	});
+
+
+
+	// pixel distance
+	var distance = $('.topbtn').offset().top;
+
+	 $(window).on('scroll', function(){
+	 	var scrolling = $(this).scrollTop();
+	});
+
+	 $(window).on('scroll', function(){
+	 	var scroll = $(this).scrollTop();
+
+	 	if(scroll >= 300){
+	 		$('.topbtn').fadeIn();
+	 	}
+	 	else
+	 		$('.topbtn').fadeOut();
+	 });
+
+	 
+	 		var distance = $('.menu').offset().top;
+	 		
+	 		$(window).on('scroll', function(){
+	 			var scroll = $(this).scrollTop();
+
+	 			if(scroll >= distance){
+	 				$('.menu').addClass('menucss');
+	 			}
+	 			else
+	 			$('.menu').removeClass('menucss');
+	 		});
+	
+
 });
